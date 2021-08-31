14449338d1a8044ba4f69e03f72ec3d69e4d3b85
MatheusTGamerPro ツ <89700288+MatheusTGamerPro@users.noreply.github.com>
2021-08-31 14:51:22 -0300

    Atualização 1/5
    - Desenvolver o Mini Game na versão MultiJogador
    
    Nota: O Código ainda está pesado e sem performance nenhuma, sendo assim no furuto irei Atualizar o Design e alterar o Tamanho do Código!

--- Jogo/GameOfLucky MultiPlayer.py
+++ Jogo/GameOfLucky MultiPlayer.py
@@ -1,2 +1,31 @@
 # Jogo no Modo: MultiJogador
-import time
+import time, random
+print("========[ Menu Inicial ]========")
+print(" Voce esta jogando no modo: MultiJogador ")
+time.sleep(2)
+print("Digite o nome dos dois jogadores que ira participar do game")
+time.sleep(1)
+player1 = input("Nome do Jogador 1: ")
+player2 = input("Nome do Jogador 2: ")
+time.sleep(2)
+print("Todos os nomes foram salvos com sucesso!")
+time.sleep(1)
+print("Vamos Iniciar seu Game...")
+while True:
+    print("Digite um valor de 1 ~ 10, os dois players teram a possibilidade de sortear um numero")
+    time.sleep(2)
+    valor1 = input("Valor do Jogador 1: ")
+    valor2 = input("Valor do Jogador 2: ")
+    maquina  = random.randint(1, 10)
+    time.sleep(2)
+    print("O Ganhador do acerto foi..")
+    time.sleep(2)
+    if valor1 == maquina:
+        print("{} Pensou em: {} \n {} Pensou em: {} \n maquina Pensou em: {}".format(player1, valor1, player2, valor2, maquina))
+        print("O Jogador {} venceu a rodada".format(player1))
+    else:
+        print("O Jogador {} nao fez nenhum acerto correto".format(player1))
+    if valor2 == maquina:
+        print("O Jogador {} venceu a rodada".format(player2))
+    else:
+        print("O Jogador {} tambem nao fez nenhum acerto".format(player2))
